963be0af83b1ecb70fde0861ce01096311644256
nyoton <dibazakaria098@gmail.com>
2024-11-21 17:30:30 +0100

    Update main.js
    update javascript threshold

--- javascript/main.js
+++ javascript/main.js
@@ -181,7 +181,7 @@ function listActivation() {
             });
         },
         {
-            threshold: 0.5 // Adjust this threshold as needed
+            threshold: 0.3
         }
     );
 
